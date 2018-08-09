784ef2602338d0a8226fdfd8902933a1f6c2399a
ambroseled <ajl190@uclive.ac.nz>
2018-08-10 10:38:36 +1200

    test case

---
+++ Fittness-Tracker-App/src/test/java/seng202.team5/CSVParserTestAmbrose.java
@@ -0,0 +1,12 @@
+package seng202.team5;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CSVParserTestAmbrose {
+    @Test
+    public void testTest() {
+        assertEquals(5, 5);
+    }
+}
