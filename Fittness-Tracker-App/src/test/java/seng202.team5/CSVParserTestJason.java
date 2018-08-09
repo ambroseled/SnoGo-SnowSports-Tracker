b3fc8c47e37ce2b2806f3a7796cb8cfc7b9aa933
Jason Little <jpl62@uclive.ac.nz>
2018-08-10 10:48:01 +1200

    fixed test

--- Fittness-Tracker-App/src/test/java/seng202.team5/CSVParserTestJason.java
+++ Fittness-Tracker-App/src/test/java/seng202.team5/CSVParserTestJason.java
@@ -1,14 +1,12 @@
 package seng202.team5;
 
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class CSVParserTestJason {
-
     @Test
-    public void test() {
-        assertEquals( expected: 9, actual: 3);
-
+    public void testTest() {
+        assertEquals(7, 7);
     }
-
 }
