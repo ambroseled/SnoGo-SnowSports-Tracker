873323549fe84a9b77875be72805890c5029131e
Viktor <vbu19@uclive.ac.nz>
2018-08-10 10:37:45 +1200

    pushing test

--- Fittness-Tracker-App/src/test/java/seng202.team5/CSVParserTest.java
+++
@@ -1,7 +0,0 @@
-package seng202.team5;
-
-import static org.junit.Assert.*;
-
-public class CSVParserTest {
-    
-}
---
+++ Fittness-Tracker-App/src/test/java/seng202.team5/CSVParserTestViktor.java
@@ -0,0 +1,12 @@
+package seng202.team5;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CSVParserTestViktor {
+    @Test
+    public void test() {
+        assertEquals(5,5);
+    }
+}
