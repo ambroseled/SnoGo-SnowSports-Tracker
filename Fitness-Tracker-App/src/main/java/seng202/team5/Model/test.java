bd3b06ef84c7c315d154c5d0f6930cecee71d19f
Ambrose Ledbrook <ajl190@uclive.ac.nz>
2018-08-19 19:54:15 +1200

    Working on the ui for the home page

--- Fitness-Tracker-App/src/main/java/seng202/team5/Model/test.java
+++ Fitness-Tracker-App/src/main/java/seng202/team5/Model/test.java
@@ -9,7 +9,7 @@ import javafx.stage.Stage;
 public class test extends Application {
 
 
-
+    @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("homePage.fxml"));
         primaryStage.setTitle("SnoGo");
