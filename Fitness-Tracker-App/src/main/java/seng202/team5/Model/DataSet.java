0dc585b38f9d20ae01846ccee423eb5a6ae4e83a
Jason Little <jasonlittle.user@gmail.com>
2018-08-28 12:51:25 +1200

    Initial data entry functionality working. Given a filename InputDataParser will create a return a list of Activity objects which have an associated DataSet which have associates DataPoints Missing date and time aspects from data points and activity

--- Fitness-Tracker-App/src/main/java/seng202/team5/Model/DataSet.java
+++ Fitness-Tracker-App/src/main/java/seng202/team5/Model/DataSet.java
@@ -1,4 +1,25 @@
 package seng202.team5.Model;
 
+import java.util.ArrayList;
+
 public class DataSet {
+
+    private ArrayList<DataPoint> dataPoints = new ArrayList<>();
+    
+    //Where the thing calculated by analysis are stored
+    private double topSpeed;
+    private double totalDistance;
+
+    public void addDataPoint(DataPoint dataPoint) {
+        dataPoints.add(dataPoint);
+    }
+
+    public String toString() {
+    	String dataPointsString = "";
+    	for (DataPoint dataPoint : dataPoints) {
+    		dataPointsString += dataPoint + "\n";
+    	}
+    	return dataPointsString;
+    }
+
 }
