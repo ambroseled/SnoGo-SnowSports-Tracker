d0a2ea032aca21e15fb436acb475978275c2c151
Unknown <damuuf@gmail.com>
2018-09-01 15:46:31 +1200

    Additional data display class edits

--- Fitness-Tracker-App/src/main/java/seng202/team5/Control/dataController.java
+++ Fitness-Tracker-App/src/main/java/seng202/team5/Control/dataController.java
@@ -1,9 +1,14 @@
 package seng202.team5.Control;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import seng202.team5.Model.Activity;
 import seng202.team5.Model.DataPoint;
@@ -15,20 +20,24 @@ import java.util.Date;
 
 public class dataController {
     private User user;
-    TableColumn<DataPoint, Date> dateTimeCol;
-
-    TableColumn<DataPoint, Integer> heartRateCol;
-    TableColumn<DataPoint, Double> latitudeCol;
-    TableColumn<DataPoint, Double> longitudeCol;
-    TableColumn<DataPoint, Double> elevationCol;
 
     public dataController(User tempUser){
         user = tempUser;
         initialise();
     }
 
+    @FXML
+    private Label usernameLabel;
+
 
     private void initialise() {
+        //Sets username on sidebar
+        usernameLabel.setText(user.getName());
+
+
+        if (user.getActivities[0]) {
+
+        }
 
     }
 
