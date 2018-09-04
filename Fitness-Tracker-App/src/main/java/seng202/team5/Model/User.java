27265aefcccf06df875f2c6eda1bb674c3c92a40
Ambrose Ledbrook <ajl190@uclive.ac.nz>
2018-09-04 17:01:37 +1200

    Making the initial user class

--- Fitness-Tracker-App/src/main/java/seng202/team5/Model/User.java
+++ Fitness-Tracker-App/src/main/java/seng202/team5/Model/User.java
@@ -1,4 +1,59 @@
 package seng202.team5.Model;
 
 public class User {
+
+    private String firstName;
+    private String lastName;
+    private int age;
+    private double height;
+    private double weight;
+
+
+    public User(String fist, String last, int age, double height, double weight) {
+        firstName = fist;
+        lastName = last;
+        this.age = age;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
 }
