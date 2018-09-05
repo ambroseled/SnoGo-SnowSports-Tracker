fb7ccac7e91a1c8b18ffb0ab24a4eba09aa8acb6
Sam Annand <sam.g.annand@gmail.com>
2018-09-06 01:15:25 +1200

    Functioning button bar to top of all semi-complete views, not quite happy with it yet

--- Fitness-Tracker-App/src/main/java/seng202/team5/Control/goalController.java
+++ Fitness-Tracker-App/src/main/java/seng202/team5/Control/goalController.java
@@ -13,4 +13,30 @@ public class goalController {
         System.out.println("Home button pressed");
         appController.changeScene("/View/mainPage.fxml", c);
     }
+
+    public void mapButtonPress() throws IOException {
+        System.out.println("Map button pressed");
+        appController.changeScene("/View/mapView.fxml", c);
+    }
+
+    public void calendarButtonPress() throws IOException {
+        System.out.println("Calendar button pressed");
+        appController.changeScene("/View/calView.fxml", c);
+    }
+
+    public void dataButtonPress() throws IOException {
+        System.out.println("Data button pressed");
+        appController.changeScene("/View/dataView.fxml", c);
+    }
+
+    public void profileButtonPress() throws IOException {
+        System.out.println("profile button pressed");
+        appController.changeScene("/View/profView.fxml", c);
+    }
+
+    public void alertsButtonPress() throws IOException {
+        System.out.println("Alerts button pressed");
+        appController.changeScene("/View/alertView.fxml", c);
+    }
+
 }
