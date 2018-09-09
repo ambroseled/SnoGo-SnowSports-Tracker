59d3c4ec4bc960534477c018ee8eedc5db089061
ambroseled <ajl190@uclive.ac.nz>
2018-09-09 14:02:13 +1200

    Merge branch 'DataBase' of /home/cosc/student/ajl190/Desktop/SENG202/SENG202-Project with conflicts.

---
+++ Fitness-Tracker-App/src/main/java/seng202/team5/Model/Goal.java
@@ -0,0 +1,65 @@
+package seng202.team5.Model;
+
+import java.text.DateFormat;
+import java.text.Format;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Goal {
+
+    private Date completionDate;
+    private String metric;
+    private double metricGoal;
+    private String name;
+    private boolean completed;
+
+
+
+    public Goal(String name, String metric, double metricGoal, String date, boolean completed) {
+        this.name = name;
+        this.metric = metric;
+        this.metricGoal = metricGoal;
+        this.completed = completed;
+        try {
+            DateFormat dateTimeFormat = new SimpleDateFormat("dd/mm/yyyy hh:mm:ss");
+            this.completionDate = dateTimeFormat.parse(date);
+        } catch (ParseException e) {
+            System.out.println("Error parsing date: " + e.getLocalizedMessage());
+        }
+
+    }
+
+
+    public Date getCompletionDate() {
+        return completionDate;
+    }
+
+    public double getMetricGoal() {
+        return metricGoal;
+    }
+
+    public String getMetric() {
+        return metric;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setCompletionData(Date completionData) {
+        this.completionDate = completionData;
+    }
+
+    public void setMetric(String metric) {
+        this.metric = metric;
+    }
+
+    public void setMetricGoal(double metricGoal) {
+        this.metricGoal = metricGoal;
+    }
+}
