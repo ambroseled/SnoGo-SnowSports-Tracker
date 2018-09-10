2291d87d77b4278ee7d892d3d512980e888b76a8
Ambrose Ledbrook <ajl190@uclive.ac.nz>
2018-09-10 13:23:06 +1200

    Removing empty lines

--- Fitness-Tracker-App/src/main/java/seng202/team5/Control/appController.java
+++ Fitness-Tracker-App/src/main/java/seng202/team5/Control/appController.java
@@ -6,9 +6,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-
 import javafx.stage.Stage;
-
 import java.io.IOException;
 import java.net.URL;
 
