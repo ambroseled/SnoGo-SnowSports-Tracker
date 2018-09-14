27255014f6b671adede0e1cd56184c9532bd67ca
Ambrose Ledbrook <ajl190@uclive.ac.nz>
2018-09-14 20:07:19 +1200

    Fixing database test case

--- Fitness-Tracker-App/src/test/java/seng202/team5/Control/DataBaseControllerTest.java
+++ Fitness-Tracker-App/src/test/java/seng202/team5/Control/DataBaseControllerTest.java
@@ -42,7 +42,7 @@ public class DataBaseControllerTest {
         User user = users.get(0);
         assertEquals("John Stevens", user.getName());
         assertEquals(25, user.getAge());
-        assertEquals(1.8, user.getHeight(), 0.0);
+        assertEquals(6.2, user.getHeight(), 0.0);
         assertEquals(75.8, user.getWeight(), 0.0);
     }
 
