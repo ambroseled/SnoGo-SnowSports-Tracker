aab530aaa94750ce66c721255c268d0d353a19c8
Sam Annand <sam.g.annand@gmail.com>
2018-09-19 14:59:44 +1200

    Trying to style error pop up

--- Fitness-Tracker-App/src/main/java/seng202/team5/Control/ErrorController.java
+++ Fitness-Tracker-App/src/main/java/seng202/team5/Control/ErrorController.java
@@ -1,6 +1,7 @@
 package seng202.team5.Control;
 
 import javafx.scene.control.Alert;
+import javafx.scene.control.DialogPane;
 
 public class ErrorController {
 
@@ -8,8 +9,11 @@ public class ErrorController {
         Alert errorPopup = new Alert(Alert.AlertType.ERROR);
         errorPopup.setTitle("Error Dialog");
         errorPopup.setContentText(errorMessage);
-
         errorPopup.showAndWait();
+
+        DialogPane dialogPane = errorPopup.getDialogPane();
+        dialogPane.getStylesheets().add("../../../res/CSS/errorStyle.css");
+        dialogPane.getStyleClass().add("error");
     }
 
 }
