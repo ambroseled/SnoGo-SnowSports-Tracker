990b68a54e18249723c9d49bb9570b603b926657
Viktor Bubanja <viktor.bubanja@hotmail.com>
2018-09-20 15:06:41 +1200

    Auto stash before revert of "Adding maven dependencies used for mapping"

--- Fitness-Tracker-App/src/main/java/seng202/team5/Control/MapController.java
+++ Fitness-Tracker-App/src/main/java/seng202/team5/Control/MapController.java
@@ -1,20 +1,22 @@
 
 package seng202.team5.Control;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
-import seng202.team5.Model.Activity;
-import seng202.team5.Model.InputDataParser;
-import seng202.team5.Model.Route;
-import seng202.team5.Model.User;
+import seng202.team5.Model.*;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class MapController implements Initializable {
+public class MapController {
     @FXML
     private ToggleGroup routeSelection;
     @FXML
@@ -31,36 +33,22 @@ public class MapController implements Initializable {
     //For testing
     User user = AppController.getCurrentUser();
 
-    @FXML
-    private WebView webView;
-
-    private WebEngine webEngine;
 
     private FXMLLoader loader = new FXMLLoader();
     private Class c = getClass();
 
 
-    private Route routeA = new Route(
-            new Position(37.772, -122.214),
-            new Position(21.291, -157.821),
-            new Position(-18.142, 178.431),
-            new Position(-27.467, 153.027)
-    );
 
-    private Route routeB = new Route(
-            new Position(-33.946111, 151.177222),
-            new Position(-43.489444, 172.532222)
-    );
-
+    /*
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         initMap();
 
         routeSelection.selectedToggleProperty().addListener((observable, oldValue, newValue) -> {
             if (newValue == routeARadioButton) {
-                displayRoute(routeA);
+                displayRoute(skiRoute);
             } else {
-                displayRoute(routeB);
+                displayRoute(new Route(g));
             }
         });
     }
@@ -73,9 +61,9 @@ public class MapController implements Initializable {
     private void displayRoute(Route newRoute) {
         String scriptToExecute = "displayRoute(" + newRoute.toJSONArray() + ");";
         webEngine.executeScript(scriptToExecute);
-    }
+    } */
 
-/*
+
 
     public void initialize() {
         setUpMap();
@@ -92,7 +80,7 @@ public class MapController implements Initializable {
     }
 
 
-    */
+
 
 
 }
