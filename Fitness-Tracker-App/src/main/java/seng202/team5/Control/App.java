230417ade7cda8483685e2a75746b7064807683a
Ambrose Ledbrook <ajl190@uclive.ac.nz>
2018-09-21 15:29:46 +1200

    Still broken, working on refactoring database

--- Fitness-Tracker-App/src/main/java/seng202/team5/Control/App.java
+++ Fitness-Tracker-App/src/main/java/seng202/team5/Control/App.java
@@ -7,6 +7,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import seng202.team5.DataManipulation.DataBaseController;
+import seng202.team5.DataManipulation.dataBase2;
 import seng202.team5.Model.User;
 import java.net.URL;
 
@@ -22,7 +23,7 @@ public class App extends Application {
     private static DataBaseController db = new DataBaseController();
     ////////////
     // Used for testing will later be the actual current user.
-    private static User currentUser;
+    private static User currentUser = db.getUsers().get(0);
     ////////////
 
 
@@ -39,7 +40,6 @@ public class App extends Application {
         primaryStage.show();
         primaryStage.setMinHeight(750);
         primaryStage.setMinWidth(1280);
-       // currentUser = db.getUsers().get(0);
     }
 
 
--- Fitness-Tracker-App/src/main/java/seng202/team5/DataManipulation/DataBaseController.java
+++ Fitness-Tracker-App/src/main/java/seng202/team5/DataManipulation/DataBaseController.java
@@ -1,5 +1,6 @@
 package seng202.team5.DataManipulation;
 
+import org.sqlite.SQLiteConfig;
 import seng202.team5.Control.App;
 import seng202.team5.Control.ErrorController;
 import seng202.team5.Model.*;
@@ -19,49 +20,144 @@ import java.util.ArrayList;
  */
 public class DataBaseController {
 
-    private Connection connection = null;
+    private static Connection connection = null;
     private SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
-    private static String dbString = "jdbc:sqlite:" + System.getProperty("user.home") + "/SnoGoDB.db";
+    private static String dbString = "jdbc:sqlite:" + System.getProperty("user.home") + "/SnoGo.db";
+    private static final String driver = "org.sqlite.JDBC";
 
 
-    /**
-     * Tries to establish a connection wih the applications database.
-     */
+    private static SQLiteConfig configureSQl() {
+        SQLiteConfig sqlConfig = new SQLiteConfig();
+        sqlConfig.enforceForeignKeys(true);
+        return sqlConfig;
+    }
+
+
     public DataBaseController() {
-        // Try-catch is used to catch any exceptions that throw while creating connection to the database
+        connectDB();
+    }
+
+
+    public static void connectDB(){
         try {
-            Class.forName("org.sqlite.JDBC");
+            Class.forName(driver);
+            //SQLiteConfig sqlConfig = configureSQl();
             connection = DriverManager.getConnection(dbString);
+            createDatabase();
         } catch (Exception e) {
-            createDB();
-            try {
-                Class.forName("org.sqlite.JDBC");
-                connection = DriverManager.getConnection(dbString);
-            } catch (Exception e2) {
-                //TODO: error dialogue
-            }
+            System.out.println("ERROR");
         }
-    }
-
-
-
-    public void createDB() {
 
     }
 
 
-    /**
-     * Closes the connections to the database
-     */
-    public void closeConnection() {
-        // Try-catch used to catch any exceptions trow while closing connection to database.
+
+
+    public static void closeConnection() {
         try {
             connection.close();
         } catch (SQLException e) {
             // Showing error dialogue to user
             ErrorController.displayError("Error closing connection to database");
         }
+
     }
+
+
+    private static void executeStmt(String query) {
+        try {
+            Statement newStmt = connection.createStatement();
+            newStmt.execute(query);
+        } catch (SQLException e) {
+
+        }
+
+    }
+
+
+    /**
+     *  Creates a new data for the application if it does not already exist and creates the databases structure (tables
+     *  and attributes). The data is stored in the project's directory and consists of four tables:
+     *  users, activities, datapoints and targets.     *
+     * @throws SQLException if an sql related problem is encountered trying to set up the data.
+     */
+    public static void createDatabase() {
+        if (connection != null) {
+            String actTable = "CREATE TABLE IF NOT EXISTS Activity (\n" +
+                    "ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,\n" +
+                    "Name VARCHAR NOT NULL,\n" +
+                    "USER INTEGER REFERENCES User (ID) ON DELETE CASCADE ON UPDATE CASCADE NOT NULL" +
+                    ");";
+
+
+            String alertTable = "CREATE TABLE IF NOT EXISTS Alert (\n" +
+                    "ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,\n" +
+                    "Type STRING NOT NULL,\n" +
+                    "MESSAGE STRING NOT NULL,\n" +
+                    "Date STRING NOT NULL,\n" +
+                    "User INTEGER NOT NULL REFERENCES User (ID) ON DELETE CASCADE ON UPDATE CASCADE NOT NULL" +
+                    ");";
+
+
+            String pointTable = "CREATE TABLE IF NOT EXISTS DataPoint (\n" +
+                    "ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,\n" +
+                    "DateTime STRING NOT NULL,\n" +
+                    "HeartRate INTEGER NOT NULL,\n" +
+                    "Latitude DOUBLE NOT NULL,\n" +
+                    "Longitude DOUBLE NOT NULL,\n" +
+                    "Elevation DOUBLE NOT NULL,\n" +
+                    "Distance DOUBLE NOT NULL,\n" +
+                    "Speed DOUBLE NOT NULL,\n" +
+                    "Active BOoLEAN NOT NULL,\n" +
+                    "DataSet INTEGER REFERENCES DataSet (ID) ON DELETE CASCADE ON UPDATE CASCADE NOT NULL" +
+                    ");";
+
+
+            String setTable = "CREATE TABLE IF NOT EXISTS DataSet (\n" +
+                    "ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,\n" +
+                    "TopSpeed DOUBLE NOT NULL,\n" +
+                    "TotalDist DOUBLE NOT NULL,\n" +
+                    "AvgHeartRate INTEGER NOT NULL,\n" +
+                    "VerticalDistance DOUBLE NOT NULL,\n" +
+                    "Calories DOUBLE NOT NULL,\n" +
+                    "SlopeTime DOUBLE NOT NULL,\n" +
+                    "AvgSpeed DOUBLE NOT NULL,\n" +
+                    "Activity INTEGER REFERENCES Activity (ID) ON DELETE CASCADE ON UPDATE CASCADE NOT NULL" +
+                    ");";
+
+
+            String goalTable = "CREATE TABLE IF NOT EXISTS Goal (\n" +
+                    "ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,\n" +
+                    "Metric STRING NOT NULL,\n" +
+                    "MetricGoal DOUBLE NOT NULL,\n" +
+                    "Name STRING NOT NULL,\n" +
+                    "Completed BOOLEAN NOT NULL,\n" +
+                    "CompletionDate STRING NOT NULL,\n" +
+                    "User INTEGER REFERENCES Activity (ID) ON DELETE CASCADE ON UPDATE CASCADE NOT NULL" +
+                    ");";
+
+
+            String userTable = "CREATE TABLE IF NOT EXISTS User (\n" +
+                    "ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,\n" +
+                    "Name STRING NOT NULL,\n" +
+                    "Height DOUBLE NOT NULL,\n" +
+                    "Weight DOUBLE NOT NULL,\n" +
+                    "Age INTEGER NOT NULL,\n" +
+                    "BirthDate STRING NOT NULL\n" +
+                    ");";
+
+
+
+            executeStmt(actTable);
+            executeStmt(alertTable);
+            executeStmt(pointTable);
+            executeStmt(setTable);
+            executeStmt(goalTable);
+            executeStmt(userTable);
+        }
+        closeConnection();
+    }
+
 
 
     /**
@@ -70,11 +166,13 @@ public class DataBaseController {
      */
     public ArrayList<User> getUsers() {
         // Creating an ArrayList to hold all the users.
-        ArrayList<User> users = new ArrayList<User>();
+        ArrayList<User> users = new ArrayList<>();
         // Try-catch is used to catch any exception that are throw wile executing the query
         try {
             // Creating a statement and then executing a query to get all users
+            System.out.println(connection);
             Statement stmt = connection.createStatement();
+            System.out.println("bean");
             ResultSet set = stmt.executeQuery("SELECT * FROM User");
             User newUser;
 
@@ -97,7 +195,7 @@ public class DataBaseController {
             }
         } catch (SQLException e) {
             // Showing error dialogue to user
-            ErrorController.displayError("Error populating the database");
+            ErrorController.displayError("Error with the database");
         }
         // Returning the ArrayList of user
         return users;
