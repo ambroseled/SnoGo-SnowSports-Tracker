312e174a7d4c513ff317f4f760a2a3d936a80601
Hayden Morriss <damuuf@gmail.com>
2018-09-24 00:08:30 +1200

    Removed redundant reset button and changed tables to fully reset in Data tab

--- Fitness-Tracker-App/src/main/java/seng202/team5/Control/TableController.java
+++ Fitness-Tracker-App/src/main/java/seng202/team5/Control/TableController.java
@@ -161,37 +161,44 @@ public class TableController {
      * @param index refers to an activity in the list "activities"
      */
     private void createTable(TableView table, int index) {
-        // date and time column
-        TableColumn<DataPoint, Date> dateTimeCol = new TableColumn("Date and Time");
-        dateTimeCol.setCellValueFactory(new PropertyValueFactory("dateTime"));
-
-        //heart rate column
-        TableColumn<DataPoint, Integer> heartRateCol = new TableColumn("Heart Rate");
-        heartRateCol.setCellValueFactory(new PropertyValueFactory("heartRate"));
-
-        //latitude column
-        TableColumn<DataPoint, Double> latitudeCol = new TableColumn("Latitude");
-        latitudeCol.setCellValueFactory(new PropertyValueFactory("latitude"));
-
-        //longitude column
-        TableColumn<DataPoint, Double> longitudeCol = new TableColumn("Longitude");
-        longitudeCol.setCellValueFactory(new PropertyValueFactory("longitude"));
-
-        //elevation column
-        TableColumn<DataPoint, Double> elevationCol = new TableColumn("Elevation");
-        elevationCol.setCellValueFactory(new PropertyValueFactory("elevation"));
-
-        //distance column
-        TableColumn<DataPoint, Double> distanceCol = new TableColumn("Distance");
-        distanceCol.setCellValueFactory(new PropertyValueFactory("distance"));
-
-        //speed column
-        TableColumn<DataPoint, Double> speedCol = new TableColumn("Speed");
-        speedCol.setCellValueFactory(new PropertyValueFactory("speed"));
 
 
-        table.getColumns().addAll(dateTimeCol, heartRateCol, latitudeCol, longitudeCol, elevationCol, distanceCol, speedCol);
-        table.setItems(getDataPointsList(index));
+        table.getItems().clear();
+        if (App.getCurrentUser() != null) {
+            if (table.getItems().size() != App.getCurrentUser().getActivities().size()) {
+                activities = db.getActivities(App.getCurrentUser().getId());
+                // date and time column
+                TableColumn<DataPoint, Date> dateTimeCol = new TableColumn("Date and Time");
+                dateTimeCol.setCellValueFactory(new PropertyValueFactory("dateTime"));
+
+                //heart rate column
+                TableColumn<DataPoint, Integer> heartRateCol = new TableColumn("Heart Rate");
+                heartRateCol.setCellValueFactory(new PropertyValueFactory("heartRate"));
+
+                //latitude column
+                TableColumn<DataPoint, Double> latitudeCol = new TableColumn("Latitude");
+                latitudeCol.setCellValueFactory(new PropertyValueFactory("latitude"));
+
+                //longitude column
+                TableColumn<DataPoint, Double> longitudeCol = new TableColumn("Longitude");
+                longitudeCol.setCellValueFactory(new PropertyValueFactory("longitude"));
+
+                //elevation column
+                TableColumn<DataPoint, Double> elevationCol = new TableColumn("Elevation");
+                elevationCol.setCellValueFactory(new PropertyValueFactory("elevation"));
+
+                //distance column
+                TableColumn<DataPoint, Double> distanceCol = new TableColumn("Distance");
+                distanceCol.setCellValueFactory(new PropertyValueFactory("distance"));
+
+                //speed column
+                TableColumn<DataPoint, Double> speedCol = new TableColumn("Speed");
+                speedCol.setCellValueFactory(new PropertyValueFactory("speed"));
+                table.getColumns().addAll(dateTimeCol, heartRateCol, latitudeCol, longitudeCol, elevationCol, distanceCol, speedCol);
+                table.setItems(getDataPointsList(index));
+                table.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+            }
+        }
     }
 
 
