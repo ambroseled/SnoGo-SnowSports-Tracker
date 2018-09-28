75e896682c7cd2a7afce197beab0ff688518f629
Sam Annand <sam.g.annand@gmail.com>
2018-09-28 14:58:12 +1200

    Adding a video file picker to the video pane

--- Fitness-Tracker-App/src/main/java/seng202/team5/Control/VideoController.java
+++ Fitness-Tracker-App/src/main/java/seng202/team5/Control/VideoController.java
@@ -1,4 +1,34 @@
 package seng202.team5.Control;
 
+import javafx.fxml.FXML;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+import javafx.stage.FileChooser;
+
+import java.io.File;
+
 public class VideoController {
+    @FXML
+    private MediaView mediaView;
+
+    private Media media;
+    private MediaPlayer mediaPlayer;
+
+    public void selectVideo() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select Video File");
+        File f = fileChooser.showOpenDialog(null);
+        try {
+            playVideo(f.getAbsolutePath());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    public void playVideo(String path) {
+
+        //yo
+    }
 }
